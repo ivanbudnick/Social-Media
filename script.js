4e5584b2797701f5c9b54e4e16cebc36d3c61d56
ivanbudnick <ivan.budnick@gmail.com>
2020-11-15 18:04:11 -0300

    Final push

--- script.js
+++ script.js
@@ -13,20 +13,36 @@ function darkMode(e) {
     clickCounter++;
     if (clickCounter%2 == 1) {
     circle.style.left = "5%";
+    circle.style.transition ="0.5s";
     outerBoxa.style.backgroundColor = "black";
     outerBox.style.backgroundColor = "black";
     outerBoxa.style.color = "white";
+    outerBox.style.transition ="0.2s";
+    outerBoxa.style.transition ="0.2s";
+    switcha.style.backgroundImage = "linear-gradient(60deg, hsl(210, 78%, 56%), hsl(146, 68%, 55%))"
+    circle.style.backgroundColor = "hsl(232, 19%, 15%)"
     for (var i=0; i<followersSquare.length; i++) {
         followersSquare[i].style.backgroundColor = "hsl(232, 19%, 15%)";
+        followersSquare[i].style.transition ="0.1s";
+
     };
     for (var j=0; j<overviewSquare.length; j++) {
         overviewSquare[j].style.backgroundColor = "hsl(232, 19%, 15%)";
+        overviewSquare[i].style.transition ="0.1s";
     };
 } else {
     circle.style.left = "50%";
+    circle.style.backgroundColor = "hsl(227, 47%, 96%)";
     outerBoxa.style.backgroundColor = "white";
     outerBox.style.backgroundColor = "white";
-    outerBoxa.style.color = "black";    
+    outerBoxa.style.color = "black";
+    switcha.style.backgroundImage = "none"
+    for (var i=0; i<followersSquare.length; i++) {
+        followersSquare[i].style.backgroundColor = "hsl(227, 47%, 96%)";
+    };
+    for (var j=0; j<overviewSquare.length; j++) {
+        overviewSquare[j].style.backgroundColor = "hsl(227, 47%, 96%)";
+    };
 }
 }
 
